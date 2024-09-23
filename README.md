54328b03fbebd41fea862d9e46fc719dfffed7f5
Siddhanta Banerjee <39611700+0xSidBanerjee@users.noreply.github.com>
2024-09-24 00:37:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Go-Bootcamp
+ 
---
+++ values.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println(true && false)
+	fmt.Println(!false)
+	fmt.Println("Random number go ", rand.Intn(2))
+}
